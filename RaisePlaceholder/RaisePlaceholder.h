832e537a92fa111ce01cfc2ea4422f4faaa73188
Lee Jiho <najanda89@gmail.com>
2017-06-14 00:36:43 +0900

    Support Carthage

---
+++ RaisePlaceholder/RaisePlaceholder.h
@@ -0,0 +1,19 @@
+//
+//  RaisePlaceholder.h
+//  RaisePlaceholder
+//
+//  Created by Lee Jiho on 2017. 6. 14..
+//  Copyright © 2017년 Lee Jiho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RaisePlaceholder.
+FOUNDATION_EXPORT double RaisePlaceholderVersionNumber;
+
+//! Project version string for RaisePlaceholder.
+FOUNDATION_EXPORT const unsigned char RaisePlaceholderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RaisePlaceholder/PublicHeader.h>
+
+
